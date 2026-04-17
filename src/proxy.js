0dd92a2cb99d7c77fd0a010e39d20f8e55fcbef9
yunus103 <yunusemre.aytekin@ogr.iuc.edu.tr>
2026-04-17 12:15:16 +0300

    Proxy setting for link in bio

--- src/proxy.js
+++ src/proxy.js
@@ -10,7 +10,7 @@ export const config = {
     // - … if they start with `/api`, `/_next` or `/_vercel`
     // - … the ones containing a dot (e.g. `favicon.ico`)
     // - ... `/studio` paths (Sanity Studio)
-    '/((?!api|_next|_vercel|studio|iletisim-kanallari|.*\\..*).*)',
+    '/((?!api|_next|_vercel|studio|iletisim-kanallari.*|.*\\..*).*)',
     // However, match all root paths
     '/',
     '/(tr|en)/:path*'
